f004650e790b873fc7b24874b2a26aa335892a0a
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-07-05 14:46:02 +0500

    add functionality to generate random numbers, and display them to html

--- js/main.js
+++ js/main.js
@@ -11,6 +11,41 @@ let selectedAlgo = 1;
 // * selecting bubble sort as default algorithn
 selectAlgorithm(selectedAlgo);
 
+let numbers = generateRandomArray(150);
+
+function generateRandomArray(size) {
+    let array = [];
+
+    for (let i = 0; i < size; i++) {
+        array.push(Math.floor(Math.random() * 500) + 1);
+    }
+
+    return array;
+}
+
+displayArray();
+
+// * display array to screen
+function displayArray() {
+    
+    let test_data_container = document.getElementById('test-data-container');
+
+    for (let i = 0; i < numbers.length; i++) {
+
+        let new_data_node = `<div id="tst-d${i}" class="test-data">
+        <div class="tooltiptext">${numbers[i]}</div>
+        </div>`;
+
+        test_data_container.innerHTML += new_data_node;
+        document.getElementById(`tst-d${i}`).style.height = numbers[i] + 'px';
+
+    }
+
+}
+
+
+
+// * for selecting and de-selecting an algorithm
 function selectAlgorithm(algoNumber) {
     
     // * 1) first re-enable the previous selected Algorithm button
