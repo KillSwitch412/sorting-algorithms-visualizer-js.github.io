ef873809750ca0a14940ec84db96aee788ab56e3
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-07-06 14:38:24 +0500

    cleaned up js file, removed algorithms file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+// ! ---------- STARTING POINT ---------- 
+// ! ---------- STARTING POINT ---------- 
+
+
 let algorithmMap = {
     1: 'Bubble Sort',
     2: 'Heap Sort',
     3: 'Insertion Sort',
     4: 'Merge Sort',
     5: 'Quick Sort',
+    6: 'Selection Sort',
 }
 
 // * default selected algorithm
@@ -16,7 +21,7 @@ let defaultSize = 60;
 let defaultMin = 1;
 let defaultMax = 100;
 
-// * default data
+// * generating and displaying new random array at every refresh
 let generatedData = generateRandomArray(defaultSize, defaultMin, defaultMax);
 var numbers = generatedData['array'];
 var largest = generatedData['largest'];
@@ -24,55 +29,29 @@ var smallest = generatedData['smallest'];
 
 displayArray(numbers, largest, smallest, defaultMin, defaultMax);
 
-// * called when user sets (new array size)
-// * the ways it works, 
-// * if user provided new size, the min and max equals the last chosen or default value
-// * 
-function setArrayAndValueConstraintsAndDisplay(size = defaultSize, min = defaultMin, max = defaultMax) {    
 
-    // * generating new array with new constraints
-    generatedData = generateRandomArray(size, min, max);
-    numbers = generatedData['array'];
-    largest = generatedData['largest'];
-    smallest = generatedData['smallest'];
 
-    clearScreen();
-    displayArray(numbers, largest, smallest, min, max);
 
-    // * setting new values
-    defaultSize = size;
-    defaultMin = min;
-    defaultMax = max;
-    
-}
 
-function generateArrayWithCustomSize() {
-    let customSize = document.getElementById('custom-size-input').value;
 
-    if (customSize > 200) {
-        return;
-    }
 
-    setArrayAndValueConstraintsAndDisplay(size = customSize);
-}
 
-// function generateArrayWithCustomRange() {
-//     let customMax = parseInt(document.getElementById('custom-range-input').value);
+// ! ---------- ARRAY GENERATING AND DISPLAY RELATED FUNCTIONS ---------- 
+// ! ---------- ARRAY GENERATING AND DISPLAY RELATED FUNCTIONS ---------- 
 
-//     setArrayAndValueConstraintsAndDisplay(size = defaultSize, min = defaultMin, max = customMax);
-// }
-
+// ! THE MAIN FUNCTION FOR GENERATING THE ARRAY
+// * returns array, largest num, smallest num
 function generateRandomArray(size = defaultSize, min = defaultMin, max = defaultMax) {
     let array = [];
     let largest = 0;
     let smallest = max;
 
     for (let i = 0; i < size; i++) {
-        let randomVal = (Math.random() * max) + 1;
+        let randomVal = (Math.random() * max) + min;
 
         // * if val greater than max
         // ? sometimes happens due to rounding
-        randomVal = randomVal > max ? randomVal -= 1 : randomVal;
+        randomVal = randomVal > max ? randomVal -= min : randomVal;
 
         array.push(randomVal);
 
@@ -94,12 +73,46 @@ function generateRandomArray(size = defaultSize, min = defaultMin, max = default
     };
 }
 
+// * called when user sets (new array size)
+// * the ways it works is that, 
+// * if user provided new size, the min and max equals default value
+// * 
+function setArrayAndValueConstraintsAndDisplay(size = defaultSize, min = defaultMin, max = defaultMax) {
+
+    // * generating new array with new constraints
+    generatedData = generateRandomArray(size, min, max);
+    numbers = generatedData['array'];
+    largest = generatedData['largest'];
+    smallest = generatedData['smallest'];
+
+    clearScreen();
+    displayArray(numbers, largest, smallest, min, max);
+
+    // * setting new values
+    defaultSize = size;
+    defaultMin = min;
+    defaultMax = max;
+
+}
+
+// * called when user sets (new array size)
+// * using the form, by entering their custom value
+function generateArrayWithCustomSize() {
+    let customSize = document.getElementById('custom-size-input').value;
+
+    if (customSize < 1 || customSize > 200) {
+        return;
+    }
+
+    setArrayAndValueConstraintsAndDisplay(size = customSize);
+}
+
 function clearScreen() {
     let test_data_container = document.getElementById('test-data-container');
     test_data_container.innerHTML = " ";
 }
 
-// * this function displays the array visualy on the screen
+// ! this function displays the array visualy on the screen
 // * with largest value taking 550px,
 // * and all others relative to it
 function displayArray(array, largest, smallest, min, max) {
@@ -132,18 +145,20 @@ function displayArray(array, largest, smallest, min, max) {
 
 }
 
+// * used for getting the padding that is applied to the bars
+// * for their width
 function getAppropriatePadding(size) {
-    
+
     if (size <= 10) {
-        return "0px 38px"        
+        return "0px 38px"
     } else if (size <= 15) {
-        return "0px 32px"        
+        return "0px 32px"
     } else if (size <= 20) {
-        return "0px 28px"        
+        return "0px 28px"
     } else if (size <= 25) {
-        return "0px 23px"        
+        return "0px 23px"
     } else if (size <= 30) {
-        return "0px 18px"        
+        return "0px 18px"
     } else if (size <= 40) {
         return "0px 14px"
     } else if (size <= 60) {
@@ -162,9 +177,34 @@ function getAppropriatePadding(size) {
 
 }
 
+// * to stop the program 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ! ---------- ALGORITHM RELATED FUNCTIONS ---------- 
+// ! ---------- ALGORITHM RELATED FUNCTIONS ---------- 
 
 
 // * for selecting and de-selecting an algorithm
@@ -190,56 +230,61 @@ function selectAlgorithm(algoNumber) {
     console.log(`selected algo-${algoNumber} (${algorithmMap[algoNumber]})`)
 }
 
+
+// ! for starting the selected algorithm
 async function startAlgo() {
 
-    // * disable start and reset button
+    // * disable start button
     let start_btn = document.getElementById('start-btn');
-    // let reset_btn = document.getElementById('reset-btn');
-
     start_btn.classList.add('disabled');
-    // reset_btn.classList.add('disabled');
-
     start_btn.classList.remove('btn-success');
-    // reset_btn.classList.remove('btn-warning');
-
     start_btn.classList.remove('active');
-
     start_btn.classList.add('btn-outline-success');
-    // reset_btn.classList.add('btn-outline-warning');
-
     start_btn.style.boxShadow = "none";
 
-    // * traverse array
-    await performBubbleSort(numbers);
-    // await bubbleSort(numbers);
+    switch (selectedAlgo) {
+        case 1:
+            await performBubbleSort(numbers);
+            break;
 
-    // * re-enable start and reset button
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        case 4:
+            break;
+
+        case 5:
+            break;
+
+        case 6:
+            break;
+    }
+
+    // * re-enable start button
     start_btn.classList.remove('disabled');
-    // reset_btn.classList.remove('disabled');
-
     start_btn.classList.remove('btn-outline-success');
-    // reset_btn.classList.remove('btn-outline-warning');
-
     start_btn.classList.add('btn-success');
-    // reset_btn.classList.add('btn-warning');
 
 }
 
 
-// ! ALGORITHMS
+// ! ---------- BUBBLE SORT ----------
 async function performBubbleSort(array) {
-    
+
     let lastSorted = array.length - 1;
     let sorted = 1;
 
     for (let times = 0; times < array.length; times++) {
 
         let swapped = false;
-        
+
         for (let i = 0; i < array.length - sorted; i++) {
             let data_node1 = document.getElementById(`tst-d${i}`);
             let data_node2 = document.getElementById(`tst-d${i + 1}`);
-            
+
             let node1_height = data_node1.style.height;
             let node2_height = data_node2.style.height;
 
@@ -249,23 +294,23 @@ async function performBubbleSort(array) {
             // * change color to black
             data_node1.style.backgroundColor = '#ffc107';
             data_node2.style.backgroundColor = '#ffc107';
-    
+
             await sleep(10);
 
             if (array[i] > array[i + 1]) {
-                
+
                 let temp = array[i];
-    
+
                 array[i] = array[i + 1];
                 data_node1.style.height = node2_height;
                 data_node1.innerHTML = node2_innerHTML;
-    
+
                 array[i + 1] = temp;
                 data_node2.style.height = node1_height;
                 data_node2.innerHTML = node1_innerHTML;
 
                 swapped = true;
-                
+
                 // * change back to normal
                 data_node1.style.backgroundColor = '#3CBEB4';
                 data_node2.style.backgroundColor = '#3CBEB4';
@@ -274,7 +319,7 @@ async function performBubbleSort(array) {
                 data_node1.style.backgroundColor = '#6610f2';
                 data_node2.style.backgroundColor = '#6610f2';
             }
-        
+
         }
 
         // if not swapped
@@ -283,7 +328,7 @@ async function performBubbleSort(array) {
         }
 
         sorted = sorted + 1;
-        
+
         document.getElementById(`tst-d${lastSorted}`).style.backgroundColor = "#6610f2";
         lastSorted = lastSorted - 1;
     }
